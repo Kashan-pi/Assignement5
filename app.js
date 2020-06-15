047bbc52612e8ea28230c3e2598593c669810de7
Kashan-pi <m.hassanshafiq@hotmail.com>
2020-06-15 20:33:27 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,108 @@
+// //Assignment #1
+
+// // Task #1
+// alert("Hellow World");
+
+// // Task #2
+// alert("Error! Please enter a valid password.");
+
+// // Task #3
+// alert("Welcome to JS Coding...\nHappy Coding!");
+
+// // Task #4
+// alert("Welcome to JS Land...");
+// alert("Happy Codding\nPrevent this page from creating additional dialogs.")
+
+// //Task #5
+// alert("Hello... I can run JS through my web browsers console ");
+
+// // Assignment #2
+
+// // Task #1
+// var username = "Kashan";
+
+// // Task #2
+// var myName;
+// myName = "Kashan Shafique";
+
+// // Task #3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// // Task #4
+// var name="Jhone Doe";
+// var age= "15 years old";
+// var description= "Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(description);
+
+// // Task #5
+// var x= "PIZZA";
+// alert(x);
+
+// // Task #6
+// var email= 'kashan.shafique96@gmail.com';
+// alert('My email address is ' +email + '.' );
+
+// //Task #7
+// var book= "“A smarter\n way to learn JavaScript";
+// alert("I am trying to learn from the book " +book + ".")
+
+// //Task #8
+// var i= "Yah! I can write HTML content through JavaScript";
+// document.write(i);
+
+// //Assignment #3
+// //Task #1
+// var age= "23";
+// alert("I am " +age+ " years old");
+
+// //Task #2
+// var n= "14";
+// alert("You have visited this site " +n+ " times");
+
+//Task #3
+// var birthyear= "1996";
+// document.write("My birth year is " +birthyear+ ".");
+
+//Task #4
+// var name= "Jon Doe";
+// var title= "T-shirt(s)";
+// var quantity= "5";
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store.");
+
+//Assignment #4
+//Task #1
+// var name= "Kashan", gender= "male", age= "23";
+
+//Task #2
+// var nameOfBand
+// var productcost
+// var name
+// var age
+// var int
+
+// var P2_a%88$
+// var Nameofband
+// var product cost
+// var 123 Kashan
+// var 234 Kas 234
+
+//Task #3
+var str = "Rules for naming JS variables";
+var result = str.bold();
+document.write(result); 
+
+
+
+
+
+
+
+
+ 
+
+
+
